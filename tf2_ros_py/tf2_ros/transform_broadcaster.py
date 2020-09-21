80ae341caca19296abf00a5e1b07811ef67d2881
Chris Lalancette <clalancette@openrobotics.org>
2020-09-21 12:13:48 +0000

    Rebase from 'upstream'

--- tf2_ros_py/tf2_ros/transform_broadcaster.py
+++ tf2_ros_py/tf2_ros/transform_broadcaster.py
@@ -29,7 +29,11 @@
 # LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
+from typing import Optional
+from typing import Union
+from typing import List
 
+from rclpy.node import Node
 from rclpy.qos import QoSProfile
 from tf2_msgs.msg import TFMessage
 from geometry_msgs.msg import TransformStamped
@@ -39,18 +43,27 @@ class TransformBroadcaster:
     """
     :class:`TransformBroadcaster` is a convenient way to send transformation updates on the ``"/tf"`` message topic.
     """
-    def __init__(self, node, qos=QoSProfile(depth=100)):
+    def __init__(
+        self,
+        node: Node,
+        qos: Optional[Union[QoSProfile, int]] = None
+    ) -> None:
         """
-        .. function:: __init__(node, qos=QoSProfile(depth=100))
+        .. function:: __init__(node, qos=None)
 
             Constructor.
 
             :param node: The ROS2 node.
             :param qos: A QoSProfile or a history depth to apply to the publisher.
         """
+        if qos is None:
+            qos = QoSProfile(depth=100)
         self.pub_tf = node.create_publisher(TFMessage, "/tf", qos)
 
-    def sendTransform(self, transform):
+    def sendTransform(
+        self,
+        transform: Union[TransformStamped, List[TransformStamped]]
+    ) -> None:
         """
         Send a transform, or a list of transforms, to the Buffer associated with this TransformBroadcaster.
 
@@ -62,5 +75,3 @@ class TransformBroadcaster:
             else:
                 transform = [transform]
         self.pub_tf.publish(TFMessage(transforms=transform))
-
-
