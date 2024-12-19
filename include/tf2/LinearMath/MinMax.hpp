c59921a51b6a65776f6daf1654b8a19095eb2288
Alejandro Hernández Cordero <ahcorde@gmail.com>
2024-12-19 13:31:02 +0100

    Rebase from 'debian/humble/tf2'

--- include/tf2/LinearMath/MinMax.hpp
+++ include/tf2/LinearMath/MinMax.hpp
@@ -13,9 +13,59 @@ subject to the following restrictions:
 */
 
 
-#ifndef TF2__LINEARMATH__TRANSFORM_H_
-#define TF2__LINEARMATH__TRANSFORM_H_
 
-#include <tf2/LinearMath/Transform.hpp>
+#ifndef TF2__LINEARMATH__MINMAX_HPP_
+#define TF2__LINEARMATH__MINMAX_HPP_
 
-#endif  // TF2__LINEARMATH__TRANSFORM_H_
+#include "Scalar.hpp"
+
+template <class T>
+TF2SIMD_FORCE_INLINE const T& tf2Min(const T& a, const T& b) 
+{
+  return a < b ? a : b ;
+}
+
+template <class T>
+TF2SIMD_FORCE_INLINE const T& tf2Max(const T& a, const T& b) 
+{
+  return  a > b ? a : b;
+}
+
+template <class T>
+TF2SIMD_FORCE_INLINE const T& GEN_clamped(const T& a, const T& lb, const T& ub) 
+{
+	return a < lb ? lb : (ub < a ? ub : a); 
+}
+
+template <class T>
+TF2SIMD_FORCE_INLINE void tf2SetMin(T& a, const T& b) 
+{
+    if (b < a) 
+	{
+		a = b;
+	}
+}
+
+template <class T>
+TF2SIMD_FORCE_INLINE void tf2SetMax(T& a, const T& b) 
+{
+    if (a < b) 
+	{
+		a = b;
+	}
+}
+
+template <class T>
+TF2SIMD_FORCE_INLINE void GEN_clamp(T& a, const T& lb, const T& ub) 
+{
+	if (a < lb) 
+	{
+		a = lb; 
+	}
+	else if (ub < a) 
+	{
+		a = ub;
+	}
+}
+
+#endif  // TF2__LINEARMATH__MINMAX_HPP_
