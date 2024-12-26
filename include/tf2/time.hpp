2d7a036ed9f025e3ebf7985749c4811a6e51fc22
Alejandro Hernández Cordero <ahcorde@gmail.com>
2024-12-27 00:31:07 +0100

    Rebase from 'release/jazzy/tf2'

--- include/tf2/time.hpp
+++ include/tf2/time.hpp
@@ -1,4 +1,4 @@
-// Copyright 2008, Willow Garage, Inc. All rights reserved.
+// Copyright 2015-2016, Open Source Robotics Foundation, Inc. All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
@@ -10,7 +10,7 @@
 //      notice, this list of conditions and the following disclaimer in the
 //      documentation and/or other materials provided with the distribution.
 //
-//    * Neither the name of the Willow Garage nor the names of its
+//    * Neither the name of the Open Source Robotics Foundation nor the names of its
 //      contributors may be used to endorse or promote products derived from
 //      this software without specific prior written permission.
 //
@@ -26,11 +26,42 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 
-/** \author Tully Foote */
+#ifndef TF2__TIME_HPP_
+#define TF2__TIME_HPP_
 
-#ifndef TF2__EXCEPTIONS_H_
-#define TF2__EXCEPTIONS_H_
+#include <chrono>
+#include <cmath>
+#include <string>
 
-#include <tf2/exceptions.hpp>
+#include "tf2/visibility_control.h"
 
-#endif  // TF2__EXCEPTIONS_H_
+namespace tf2
+{
+using Duration = std::chrono::nanoseconds;
+using TimePoint = std::chrono::time_point<std::chrono::system_clock, Duration>;
+
+using IDuration = std::chrono::duration<int, std::nano>;
+// This is the zero time in ROS
+static const TimePoint TimePointZero = TimePoint(IDuration::zero());
+
+TF2_PUBLIC
+TimePoint get_now();
+
+TF2_PUBLIC
+Duration durationFromSec(double t_sec);
+
+TF2_PUBLIC
+TimePoint timeFromSec(double t_sec);
+
+TF2_PUBLIC
+double durationToSec(const tf2::Duration & input);
+
+TF2_PUBLIC
+double timeToSec(const TimePoint & timepoint);
+
+TF2_PUBLIC
+std::string displayTimePoint(const TimePoint & stamp);
+
+}  // namespace tf2
+
+#endif  // TF2__TIME_HPP_
