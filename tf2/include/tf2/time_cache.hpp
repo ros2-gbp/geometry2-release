dd982332c4e79be2eff99fe19041241de51259ca
Alejandro Hernández Cordero <ahcorde@gmail.com>
2025-01-14 15:43:29 +0100

    Imported upstream version '0.25.11' of 'upstream'

--- tf2/include/tf2/time_cache.hpp
+++ tf2/include/tf2/time_cache.hpp
@@ -39,7 +39,6 @@
 
 #include "tf2/visibility_control.h"
 #include "tf2/transform_storage.hpp"
-#include "tf2/exceptions.hpp"
 
 namespace tf2
 {
@@ -57,7 +56,7 @@ public:
   TF2_PUBLIC
   virtual bool getData(
     tf2::TimePoint time, tf2::TransformStorage & data_out,
-    std::string * error_str = 0, TF2Error * error_code = 0) = 0;
+    std::string * error_str = 0) = 0;
 
   /** \brief Insert data into the cache */
   TF2_PUBLIC
@@ -69,8 +68,7 @@ public:
 
   /** \brief Retrieve the parent at a specific time */
   TF2_PUBLIC
-  virtual CompactFrameID getParent(
-    tf2::TimePoint time, std::string * error_str = 0, TF2Error * error_code = 0) = 0;
+  virtual CompactFrameID getParent(tf2::TimePoint time, std::string * error_str) = 0;
 
   /**
    * \brief Get the latest time stored in this cache, and the parent associated with it.  Returns parent = 0 if no data.
@@ -105,6 +103,14 @@ constexpr tf2::Duration TIMECACHE_DEFAULT_MAX_STORAGE_TIME = std::chrono::second
 class TimeCache : public TimeCacheInterface
 {
 public:
+  /// Number of nano-seconds to not interpolate below.
+  TF2_PUBLIC
+  static const int MIN_INTERPOLATION_DISTANCE = 5;
+
+  /// Maximum length of linked list, to make sure not to be able to use unlimited memory.
+  TF2_PUBLIC
+  static const unsigned int MAX_LENGTH_LINKED_LIST = 1000000;
+
   TF2_PUBLIC
   explicit TimeCache(tf2::Duration max_storage_time = TIMECACHE_DEFAULT_MAX_STORAGE_TIME);
 
@@ -113,14 +119,13 @@ public:
   TF2_PUBLIC
   virtual bool getData(
     tf2::TimePoint time, tf2::TransformStorage & data_out,
-    std::string * error_str = 0, TF2Error * error_code = 0);
+    std::string * error_str = 0);
   TF2_PUBLIC
   virtual bool insertData(const tf2::TransformStorage & new_data);
   TF2_PUBLIC
   virtual void clearList();
   TF2_PUBLIC
-  virtual tf2::CompactFrameID getParent(
-    tf2::TimePoint time, std::string * error_str = 0, TF2Error * error_code = 0);
+  virtual tf2::CompactFrameID getParent(tf2::TimePoint time, std::string * error_str);
   TF2_PUBLIC
   virtual P_TimeAndFrameID getLatestTimeAndParent();
 
@@ -150,7 +155,7 @@ private:
   // Assumes storage is already locked for it
   inline uint8_t findClosest(
     tf2::TransformStorage * & one, TransformStorage * & two,
-    tf2::TimePoint target_time, std::string * error_str = 0, TF2Error * error_code = 0);
+    tf2::TimePoint target_time, std::string * error_str);
 
   inline void interpolate(
     const tf2::TransformStorage & one, const tf2::TransformStorage & two,
@@ -164,17 +169,14 @@ class StaticCache : public TimeCacheInterface
 public:
   /// Virtual methods
   TF2_PUBLIC
-  virtual bool getData(
-    TimePoint time, TransformStorage & data_out,
-    std::string * error_str = 0, TF2Error * error_code = 0);
+  virtual bool getData(TimePoint time, TransformStorage & data_out, std::string * error_str = 0);
   // returns false if data unavailable (should be thrown as lookup exception
   TF2_PUBLIC
   virtual bool insertData(const TransformStorage & new_data);
   TF2_PUBLIC
   virtual void clearList();
   TF2_PUBLIC
-  virtual CompactFrameID getParent(
-    TimePoint time, std::string * error_str = 0, TF2Error * error_code = 0);
+  virtual CompactFrameID getParent(TimePoint time, std::string * error_str);
   TF2_PUBLIC
   virtual P_TimeAndFrameID getLatestTimeAndParent();
 
