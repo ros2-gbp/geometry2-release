e06c588055c8af3f9ab893c150273910f01c5c03
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2026-09-15 10:20:18 +0200

    Rebase from 'upstream'

--- tf2_ros/src/static_transform_broadcaster_program.cpp
+++ tf2_ros/src/static_transform_broadcaster_program.cpp
@@ -40,7 +40,11 @@
 #include "tf2/LinearMath/Quaternion.hpp"
 #include "tf2/LinearMath/Vector3.hpp"
 
-#include "rclcpp/rclcpp.hpp"
+#include "rclcpp/executors.hpp"
+#include "rclcpp/logger.hpp"
+#include "rclcpp/logging.hpp"
+#include "rclcpp/node_options.hpp"
+#include "rclcpp/utilities.hpp"
 
 struct Option
 {
@@ -300,7 +304,9 @@ int main(int argc, char ** argv)
 
   std::string ret = parse_args(args, help, rotation, translation, frame_id, child_frame_id);
   if (ret != "") {
-    RCUTILS_LOG_ERROR("error parsing command line arguments: %s", ret.c_str());
+    RCLCPP_ERROR(
+      rclcpp::get_logger("static_transform_publisher"),
+      "error parsing command line arguments: %s", ret.c_str());
     print_usage();
     return 1;
   }
